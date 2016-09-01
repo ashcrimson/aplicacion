b9d14ad4f8f107f96fe2ffee41587238805f611b
ashcrimson <felipepinoredes@gmail.com>
2016-09-01 15:14:37 -0400

    sube

---
+++ application/controllers/home.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* 
+*/
+class Home extends CI_Controller
+{
+	
+	public function index(){
+
+		$data = array('titulo' => 'Home');
+		$this->load->view("guest/head", $data);
+
+		$data = array ('app' => 'Blog');
+		$this->load->view("guest/nav", $data);
+
+		$data = array('post' => 'Blog', 'descripcion' => 'Bienvenido a mi página web con CodeIgniter');
+		$this->load->view("guest/header", $data);
+
+		$result = $this->post->getPost();
+
+		$data = array('consulta' => $result);
+		$this->load->view("guest/content", $data);
+		$this->load->view("guest/footer");
+
+
+
+	}
+}
+
+?>
---
+++ application/views/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	
+	<title><?= $title ?></title>
+</head>
+<body>
+	<div class="col-lg-12">
+	<button class="btn btn-primary">Aceptar</button>
+		<h1><?= $mensaje ?></h1>
+
+	</div>
+</body>
+</html>
